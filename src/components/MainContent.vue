29f98c4316f23686757962fecdb8477e3101ee7f
rodrigobpe <rodrigobatistapereira2@gmail.com>
2023-05-01 19:05:43 -0300

    feat: arrumando perfil

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,22 +1,37 @@
 <script setup>
+import perfil from '/public/perfil.png'
 </script>
 
 <template>
   <main class="main-container">
-    <section class="main-section"></section>
+    <section class="main-section">
+      <img :src="perfil" alt="perfil" class="main-perfil">
+    </section>
   </main>
 </template>
 
 
 <style>
 .main-container {
+  margin-top: 5rem ;
   display: flex;
   justify-content: center;
 }
 
 .main-section {
-  background-color: var(--color-positive);
+  display: flex;
+  justify-content: center;
+  align-items: start;
+  border-radius: 2.5rem;
+  background-color: var(--color-negative);
   width: 60%;
-  height: 92vh;
+  height: 800px;
+}
+
+.main-perfil{
+  margin: 4rem 0;
+  border: 3px solid var(--color-positive);
+  border-radius: 50%;
+  width: 20rem;
 }
 </style>
